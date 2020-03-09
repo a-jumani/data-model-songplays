b970080e63020243c905e0867ba23ce34f0be933
a-jumani <arsalan.jumani@outlook.com>
2020-03-09 16:40:52 +0400

    feat: Iterate over data files and process them

--- etl.py
+++ etl.py
@@ -1,4 +1,6 @@
 import argparse
+import glob
+import os
 import pandas as pd
 import psycopg2
 import textwrap
@@ -99,7 +101,22 @@ def process_data(cur, conn, filepath, func):
     Returns:
         None
     """
-    pass
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
 
 
 if __name__ == "__main__":
